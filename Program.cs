2ea247474955c2d7d1c9d5c06cc8357bdd74c9f1
Lutz <florian.lutz@zeiss.com>
2021-11-30 14:10:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TestClientAuthCert
+{
+    class Program
+    {
+        static HttpClientHandler handler = new HttpClientHandler();
+        static readonly string CERTIFICATEFILEPATH = "../../../certificate.pfx";
+        static readonly string PASSWORDFILEPATH = "../../../pwd.txt";
+        static readonly string ENDPONINT = "https://pki.zeiss.org/pkiWebDev/test";
+
+
+        static void Main()
+        {
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            var certificate = new X509Certificate2(CERTIFICATEFILEPATH, System.IO.File.ReadAllText(PASSWORDFILEPATH));
+            handler.ClientCertificates.Add(certificate);
+            var client = new HttpClient(handler);
+            CallEndpointAsync(client).Wait();
+            Console.ReadKey();
+        }
+
+        static async Task CallEndpointAsync(HttpClient client)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(ENDPONINT);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Exception Caught!");
+                Console.WriteLine("Message: {0} ", e.Message);
+            }
+        }
+    }
+}
